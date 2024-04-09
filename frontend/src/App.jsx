7f955c424155fa06fb0132b5b9f3347d851c500f
K.M.S.Dulshan <sumudu.dkm@gmail.com>
2024-04-09 22:44:37 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import AppRoutes from './routes/app-routes';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<div className='main-content'>
+				<AppRoutes />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,28 @@
+import Home from './static/home';
+
+import SignIn from './signin/signin';
+import SignUp from './signup/signup';
+
+import InventoryDashboard from './inventory/dashboard';
+import AddBlood from './inventory/add-blood';
+import GetBlood from './inventory/inventory';
+import UpdateBlood from './inventory/update-blood';
+
+import RequestDashboard from './request/dashboard';
+import NewRequest from './request/new-request';
+import GetRequests from './request/requests';
+import UpdateRequest from './request/update-request';
+
+export {
+	Home,
+	SignIn,
+	SignUp,
+	AddBlood,
+	GetRequests,
+	InventoryDashboard,
+	GetBlood,
+	UpdateBlood,
+	RequestDashboard,
+	NewRequest,
+	UpdateRequest
+};
