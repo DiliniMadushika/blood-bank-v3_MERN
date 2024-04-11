076fd40475e34becb74762fdfafc003c50029f16
K.M.S.Dulshan <sumudu.dkm@gmail.com>
2024-04-11 14:34:24 +0530

    Footer design

---
+++ frontend/src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<div className='bottom-0 w-100'>
+			<footer className='bg-danger text-white text-center pt-4 d-flex align-items-center justify-content-center'>
+				<p>Copyright © Blood Reservations</p>
+			</footer>
+		</div>
+	);
+};
+
+export default Footer;
